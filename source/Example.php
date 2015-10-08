c9d7cf95d1c4b02fa9735468af27fdfc5f621000
Goran Košutić <gkosutic@tofex.de>
2015-10-08 14:32:05 +0200

    phpunit & ide setup files

---
+++ source/Example.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace AngularPress;
+
+
+class Example {
+
+}
---
+++ tests/ExampleTest.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace AngularPress\Tests;
+
+class ExampleTest extends \PHPUnit_Framework_TestCase {
+
+    public function test() {
+
+        $t = new \AngularPress\Example();
+        $this->assertTrue(true);
+    }
+}
