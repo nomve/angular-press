fe0c954d0beb51c07a9c6c0f47ecadd2b27d941e
Goran Košutić <gkosutic@tofex.de>
2015-10-09 14:53:31 +0200

    switch to wp_mock for testing, standard testing framework is too slow

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -2,11 +2,17 @@
 
 namespace AngularPress\Tests;
 
-class ExampleTest extends \WP_UnitTestCase {
+class ExampleTest extends \PHPUnit_Framework_TestCase {
+
+    public function setUp() {
+        \WP_Mock::setUp();
+    }
+
+    public function tearDown() {
+        \WP_Mock::tearDown();
+    }
 
     public function test() {
-
-        $t = new \AngularPress\Example();
-        $this->assertTrue(true);
+        $this->assertFalse(false);
     }
 }
