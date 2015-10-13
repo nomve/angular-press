21eeb453c551a99fb7746d18edc61bed1ded6cd9
Goran Košutić <kosutic.goran@gmail.com>
2015-10-13 17:37:53 +0200

    register hooks to add extra fields in json response

--- source/classes/Plugin.php
+++ source/classes/Plugin.php
@@ -5,14 +5,41 @@ namespace AngularPress;
 
 
 class Plugin {
+    
+    function __construct() {
+        
+        add_action( 'rest_api_init', array($this, 'registerPostField') );
+    }
 
     public function pluginActivation() {
-
-
+        // doesn't save settings at the moment
     }
 
     public function pluginDeactivation() {
-
-
+        // no settings atm
     }
+    
+    /**
+     * registers a field in the post content field
+     */
+    public function registerPostField() {
+        
+        register_api_field( 'post',
+            'content',
+            array(
+                'get_callback'    => array(
+                    $this,
+                    'addAngularPostContentField'
+                ),
+                'update_callback' => null,
+                'schema'          => null,
+            ) 
+        ); 
+    }
+    /**
+     * adds a field in the post content
+     */
+    public function addAngularPostContentField() {
+    }
+    
 }
--- tests/PluginTest.php
+++ tests/PluginTest.php
@@ -6,6 +6,56 @@ namespace AngularPress\Tests;
 
 class PluginTest extends \WP_Mock\Tools\TestCase {
 
-    public function testActivationActions() {
+    /**
+     * 
+     */
+    public function testRegisteringCallbackToAddJsonField() {
+        
+        \WP_Mock::expectActionAdded( 'rest_api_init', array(
+                new \AngularPress\Plugin,
+                'registerPostField'
+            )
+        );
+        
+        new \AngularPress\Plugin();
+    }
+    
+    /**
+     * 
+     */
+    public function testRegisteringFieldToPostContent() {
+        
+        $pluginInstance = new \AngularPress\Plugin;
+
+        \WP_Mock::wpFunction(
+            'register_api_field',
+            array(
+                'times' => 1,
+                'args' => array(
+                    'post',
+                    'content',
+                    array(
+                        'get_callback'    => array(
+                            $pluginInstance,
+                            'addAngularPostContentField'
+                        ),
+                        'update_callback' => null,
+                        'schema'          => null,
+                    )  
+                ),
+            )
+        );
+        
+        $pluginInstance->registerPostField();
+    }
+    
+    /**
+     * 
+     */
+    public function testAddingAngularJsonFieldToPostContent() {
+        
+        $pluginInstance = new \AngularPress\Plugin;
+        
+        $pluginInstance->addAngularPostContentField();
     }
 }
